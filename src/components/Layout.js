beef871c50bbeac244eaa5571ba6e2c8d9ec55d3
Angel Sullon <asullom@gmail.com>
2017-08-15 10:52:08 -0500

    x

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Footer from './Footer';
+import Header from './Header';
 
 class Layout extends Component {
+  upTitle(e){
+    const title = e.target.value
+    console.log("title:"+title)
+  }
   render() {
     return (
       <div>
-        <div>Header</div>
-        <div>Content Layout</div>
+        <Header title ="Hola"/>
+        <input type="text" onChange ={this.upTitle.bind(this)} />
         <Footer />
       </div>
     );
