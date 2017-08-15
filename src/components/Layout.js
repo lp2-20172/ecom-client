01a11eab4103f4c71c132417430318e8b99f19b8
Angel Sullon <asullom@gmail.com>
2017-08-15 10:56:21 -0500

    final

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,24 @@ import Footer from './Footer';
 import Header from './Header';
 
 class Layout extends Component {
-  upTitle(e){
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Holass"
+    }
+  }
+  upTitle(e) {
     const title = e.target.value
-    console.log("title:"+title)
+    console.log("title:" + title)
+    this.setState({
+        title: title
+      })
   }
   render() {
     return (
       <div>
-        <Header title ="Hola"/>
-        <input type="text" onChange ={this.upTitle.bind(this)} />
+        <Header title={this.state.title} />
+        <input type="text" onChange={this.upTitle.bind(this)} />
         <Footer />
       </div>
     );
