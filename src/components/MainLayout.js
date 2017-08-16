750379e887c400aa8acf5eb682739a9aedb1df93
Angel Sullon <asullom@gmail.com>
2017-08-16 16:19:40 -0500

    ...

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router'
+import { Link } from 'react-router'
+
 
 class MainLayout extends Component {
-  render() {
-    return (
-      <div className="layout">
-          <div className="menu">
-            <ul>
-                <li>
-                    <Link to="/" >Home</Link>
-                    <Link to="/users" >User List</Link>
-                </li>
-                </ul>
+    render() {
+        return (
+            <div className="layout">
+                <div className="menu">
+                    <ul>
+                        <li><Link to="/" >Home</Link></li>
+                        <li><Link to="/users" >User List</Link></li>
+                    </ul>
 
-          </div>
-          <div className="content">
-                Soy el main body 
+                </div>
+                <div className="content">
+                    Soy el main body
                 {this.props.children}
-          </div>
-          
-      </div>
-    );
-  }
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default MainLayout;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Layout from './components/Layout';
+import MainLayout from './components/MainLayout';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, browserHistory } from 'react-router'
+import Footer from './components/Footer';
+import Layout from './components/Layout';
 
 ReactDOM.render(
-<Layout />, 
-document.getElementById('root'));
+    <Router history={browserHistory}>
+        <Route component={MainLayout}>
+            <Route path="/" component={Footer}/>
+            <Route path="/users" component={Layout}/>
+        </Route>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
